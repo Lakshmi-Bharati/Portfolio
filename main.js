ba5fedfd55b287bc8c7417deeeae1a32e5ce5057
Lakshmi-Bharati <lakshmibharatijuly96@gmail.com>
2024-03-17 14:53:58 +0530

    ....

---
+++ main.js
@@ -0,0 +1,8 @@
+var typing = new Typed(".text", 
+{
+    strings: ["Full Stack Java Developer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
